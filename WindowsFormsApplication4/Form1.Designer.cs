245e59cff84f72fa64fef5609f75f7943ff003f9
Val Osipenko <valosip@gmail.com>
2015-09-22 11:20:42 -0500

    adding full public tc project

---
+++ WindowsFormsApplication4/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.useridBox = new System.Windows.Forms.TextBox();
+            this.cookieBox = new System.Windows.Forms.TextBox();
+            this.userNameLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.questionsRatedLabel = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.status = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(12, 246);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // useridBox
+            // 
+            this.useridBox.Location = new System.Drawing.Point(12, 41);
+            this.useridBox.Name = "useridBox";
+            this.useridBox.Size = new System.Drawing.Size(163, 20);
+            this.useridBox.TabIndex = 1;
+            this.useridBox.Text = "UserID";
+            this.useridBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // cookieBox
+            // 
+            this.cookieBox.Location = new System.Drawing.Point(181, 41);
+            this.cookieBox.Name = "cookieBox";
+            this.cookieBox.Size = new System.Drawing.Size(320, 20);
+            this.cookieBox.TabIndex = 2;
+            this.cookieBox.Text = "ap_session=";
+            this.cookieBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // userNameLabel
+            // 
+            this.userNameLabel.AutoSize = true;
+            this.userNameLabel.BackColor = System.Drawing.Color.Transparent;
+            this.userNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userNameLabel.ForeColor = System.Drawing.Color.Black;
+            this.userNameLabel.Location = new System.Drawing.Point(27, 13);
+            this.userNameLabel.Name = "userNameLabel";
+            this.userNameLabel.Size = new System.Drawing.Size(121, 25);
+            this.userNameLabel.TabIndex = 3;
+            this.userNameLabel.Text = "UserName";
+            this.userNameLabel.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(229, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(194, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Questions Rated:";
+            this.label2.Visible = false;
+            // 
+            // questionsRatedLabel
+            // 
+            this.questionsRatedLabel.BackColor = System.Drawing.Color.Transparent;
+            this.questionsRatedLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.questionsRatedLabel.Location = new System.Drawing.Point(429, 13);
+            this.questionsRatedLabel.Name = "questionsRatedLabel";
+            this.questionsRatedLabel.Size = new System.Drawing.Size(65, 25);
+            this.questionsRatedLabel.TabIndex = 5;
+            this.questionsRatedLabel.Text = "0";
+            this.questionsRatedLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.questionsRatedLabel.Visible = false;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.Location = new System.Drawing.Point(380, 240);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(0, 31);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.Location = new System.Drawing.Point(139, 141);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(0, 13);
+            this.status.TabIndex = 8;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(181, 246);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(163, 28);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "QUIT";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(512, 278);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.questionsRatedLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.userNameLabel);
+            this.Controls.Add(this.cookieBox);
+            this.Controls.Add(this.useridBox);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Trivia Crack: Rate Questions ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox useridBox;
+        private System.Windows.Forms.TextBox cookieBox;
+        private System.Windows.Forms.Label userNameLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label questionsRatedLabel;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label status;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Web.UI;
+using System.Web.Script.Serialization;
+using System.IO;
+using Newtonsoft.Json;
+using System.Threading;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            useridBox.Visible = false;
+            cookieBox.Visible = false;
+            userNameLabel.Visible = true;
+            label2.Visible = true;
+            questionsRatedLabel.Visible = true;
+            int count = 0;
+            int countlimit = 1000;
+            GetUsername();
+                while (count <= countlimit)
+                {
+
+                        GetAchievements();
+                        RateQuestion();
+                        Thread.Sleep(500);
+                }
+        }
+        
+        private void VisitLink()
+        {
+            linkLabel1.LinkVisited = true;
+            System.Diagnostics.Process.Start("http://www.google.com");
+        }
+        public class GetQuestion {
+            public int id { get; set; }
+            public string category { get; set; }
+            public string text { get; set; }
+            public List<string> answers { get; set; }
+            public int correct_answer { get; set; }
+            public string media_type { get; set; }
+        }
+        public delegate void SetTextDelegate(System.Windows.Forms.Control ctrl, string text);
+        public static void SetText2(System.Windows.Forms.Control ctrl, string text)
+        {
+
+            if (ctrl.InvokeRequired)
+            {
+                object[] params_list = new object[] { ctrl, text };
+                ctrl.Invoke(new SetTextDelegate(SetText2), params_list);
+            }
+            else
+            {
+                ctrl.Text = text;
+            }
+            Application.DoEvents();
+        }
+        public class GetAchieve
+        {
+            public int id { get; set; }
+            public string status { get; set; }
+            public int value { get; set; }
+            public int counter { get; set; }
+            public int percent { get; set; }
+            public bool disabled { get; set; }
+            public string reward_type { get; set; }
+            public int rewards { get; set; }
+        }
+        public class Podium
+        {
+            public int first_place { get; set; }
+            public int second_place { get; set; }
+            public int third_place { get; set; }
+        }
+
+        public class Rankings
+        {
+            public Podium podium { get; set; }
+        }
+
+        public class Challenges
+        {
+            public int won { get; set; }
+            public int lost { get; set; }
+        }
+
+        public class CategoryQuestion
+        {
+            public string category { get; set; }
+            public int correct { get; set; }
+            public int incorrect { get; set; }
+            public bool worst { get; set; }
+        }
+
+        public class LanguageQuestion
+        {
+            public string language { get; set; }
+            public int correct { get; set; }
+            public int incorrect { get; set; }
+        }
+
+        public class Statistics
+        {
+            public Rankings rankings { get; set; }
+            public Challenges challenges { get; set; }
+            public IList<CategoryQuestion> category_questions { get; set; }
+            public IList<LanguageQuestion> language_questions { get; set; }
+            public int games_won { get; set; }
+            public int games_lost { get; set; }
+            public int games_resigned { get; set; }
+            public int duel_games_won { get; set; }
+            public int duel_games_lost { get; set; }
+            public int consecutive_games_won { get; set; }
+            public int consecutive_answers_correct { get; set; }
+        }
+
+        public class LevelData
+        {
+            public int level { get; set; }
+            public int points { get; set; }
+            public int progress { get; set; }
+            public int goal_points { get; set; }
+            public bool level_up { get; set; }
+        }
+
+        public class Profile
+        {
+            public bool is_friend { get; set; }
+            public string phone { get; set; }
+            public string zip_code { get; set; }
+            public IList<object> buddiesFacebookId { get; set; }
+            public Statistics statistics { get; set; }
+            public int id { get; set; }
+            public bool is_blocked { get; set; }
+            public bool allow_og_posts { get; set; }
+            public string username { get; set; }
+            public int friends { get; set; }
+            public string description { get; set; }
+            public int blocked_users_count { get; set; }
+            public LevelData level_data { get; set; }
+            public bool has_pass { get; set; }
+            public bool is_app_user { get; set; }
+            public IList<object> games_by_language { get; set; }
+            public string online_status { get; set; }
+            public bool fb_show_name { get; set; }
+            public string last_play_date { get; set; }
+            public string photo_url { get; set; }
+            public string last_log { get; set; }
+            public bool fb_show_picture { get; set; }
+            public string twitter_name { get; set; }
+            public string country { get; set; }
+            public string email { get; set; }
+        }
+        public void GetUsername()
+        {
+            string userid = useridBox.Text;
+            string cookie = cookieBox.Text;
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://api.preguntados.com/api/users/" + userid + "/profiles/"+userid);
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Accept = "*/*";
+            httpWebRequest.Method = "GET";
+            httpWebRequest.UserAgent = "Preguntados/1.9.2 (iPhone; iOS 8.1.2; Scale/2.00)";
+            httpWebRequest.Headers.Add("Accept-Language", "en-us");
+            httpWebRequest.Headers.Add("Accept-Encoding", "gzip, deflate");
+            httpWebRequest.Headers.Add("Eter-Agent", "1|iOS-AppStr|iPhone6,1|0|iOS 8.1.2|0|1.9.2|fr|fr|CA|1");
+            httpWebRequest.CookieContainer = new CookieContainer();
+            httpWebRequest.CookieContainer.Add(new Uri("http://api.preguntados.com/api/users/" + userid + "/profiles/" + userid),
+                new Cookie("ap_session", cookie));
+
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+
+            using (var reader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                JavaScriptSerializer js = new JavaScriptSerializer();
+                var objText = reader.ReadToEnd();
+
+
+                Profile result = JsonConvert.DeserializeObject<Profile>(objText);
+                //var items = JsonConvert.DeserializeObject<List<GetAchieve>>(objText);
+
+                string username = result.username;
+               // MessageBox.Show(username);
+                userNameLabel.Text = username;
+                SetText2(userNameLabel, username);
+                
+                //MessageBox.Show(QuestionCount.ToString());
+            }
+        }
+        public void GetAchievements()
+        {
+            string userid = useridBox.Text;
+            string cookie = cookieBox.Text;
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://api.preguntados.com/api/users/" + userid + "/achievements");
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Accept = "*/*";
+            httpWebRequest.Method = "GET";
+            httpWebRequest.UserAgent = "Preguntados/1.9.2 (iPhone; iOS 8.1.2; Scale/2.00)";
+            httpWebRequest.Headers.Add("Accept-Language", "en-us");
+            httpWebRequest.Headers.Add("Accept-Encoding", "gzip, deflate");
+            httpWebRequest.Headers.Add("Eter-Agent", "1|iOS-AppStr|iPhone6,1|0|iOS 8.1.2|0|1.9.2|fr|fr|CA|1");
+            httpWebRequest.CookieContainer = new CookieContainer();
+            httpWebRequest.CookieContainer.Add(new Uri("http://api.preguntados.com/api/users/" + userid + "/achievements"),
+                new Cookie("ap_session", cookie));
+            
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+
+            using (var reader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                JavaScriptSerializer js = new JavaScriptSerializer();
+                var objText = reader.ReadToEnd();
+
+
+                GetAchieve[] result = JsonConvert.DeserializeObject<GetAchieve[]>(objText);
+                var items = JsonConvert.DeserializeObject<List<GetAchieve>>(objText);
+                
+
+                int QuestionCount = result[45].counter;
+                questionsRatedLabel.Text = QuestionCount.ToString();
+                SetText2(questionsRatedLabel, QuestionCount.ToString());
+                if (QuestionCount >= 1000)
+                {
+                    MessageBox.Show("You've successfully rated 1000 questions,\n                            EXITING");
+                    Environment.Exit(0); 
+                }
+                
+                //MessageBox.Show(QuestionCount.ToString());
+            }
+        }
+
+        public void RateQuestion()
+        {
+            string userid = useridBox.Text;
+            string cookie = cookieBox.Text;
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://api.preguntados.com/api/users/" + userid + "/question-rating?country=GX&lang=EN");
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Accept = "*/*";
+            httpWebRequest.Method = "GET";
+            httpWebRequest.UserAgent = "Preguntados/1.9.2 (iPhone; iOS 8.1.2; Scale/2.00)";
+            httpWebRequest.Headers.Add("Accept-Language", "en-us");
+            httpWebRequest.Headers.Add("Accept-Encoding", "gzip, deflate");
+            httpWebRequest.Headers.Add("Eter-Agent", "1|iOS-AppStr|iPhone6,1|0|iOS 8.1.2|0|1.9.2|fr|fr|CA|1");
+            httpWebRequest.CookieContainer = new CookieContainer();
+            httpWebRequest.CookieContainer.Add(new Uri("http://api.preguntados.com/api/users/" + userid + "/question-rating?country=GX&lang=EN"),
+                new Cookie("ap_session", cookie));
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+
+            using (var reader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                JavaScriptSerializer js = new JavaScriptSerializer();
+                var objText = reader.ReadToEnd();
+                GetQuestion myojb = (GetQuestion)js.Deserialize(objText, typeof(GetQuestion));
+                string answer = "{\"id\":\"" + myojb.id + "\",\"language\":\"EN\",\"vote\":\"POSITIVE\"}";
+                // MessageBox.Show(answer);
+                sendAnswer(answer, userid);
+            }
+        }
+
+        public void sendAnswer(string answer, string userid)
+        {
+            string cookie = cookieBox.Text;
+            string post_data = answer;
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://api.preguntados.com/api/users/" + userid + "/question-rating?country=GX&lang=EN");
+            //httpWebRequest.ContentType = "application/json";
+            //httpWebRequest.Accept = "*/*";
+            httpWebRequest.Method = "POST";
+            httpWebRequest.UserAgent = "Preguntados/1.9.2 (iPhone; iOS 8.1.2; Scale/2.00)";
+            httpWebRequest.ContentType = "application/json; charset=utf-8";
+            httpWebRequest.Headers.Add("Accept-Language", "en-us");
+            httpWebRequest.Headers.Add("Accept-Encoding", "gzip, deflate");
+            httpWebRequest.Headers.Add("Eter-Agent", "1|iOS-AppStr|iPhone6,1|0|iOS 8.1.2|0|1.9.2|fr|fr|CA|1");
+            httpWebRequest.CookieContainer = new CookieContainer();
+            httpWebRequest.CookieContainer.Add(new Uri("http://api.preguntados.com/api/users/" + userid + "/question-rating?country=GX&lang=EN"),
+                new Cookie("ap_session", cookie));
+            byte[] postBytes = Encoding.ASCII.GetBytes(post_data);
+            httpWebRequest.ContentLength = postBytes.Length;
+
+            Stream requestStream = httpWebRequest.GetRequestStream();
+            requestStream.Write(postBytes, 0, postBytes.Length);
+            requestStream.Close();
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            try
+            {
+                VisitLink();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unable to open link that was clicked.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+        
+    }
+}
